56c5f936b856b5b8f05cb825a4f17ec1ba227696
Clara Raef Nasry <clara180550@feng.bu.edu.eg>
2022-01-06 00:28:14 +0200

    Update wzip.c

--- Part 1/wzip & wunzip/wzip.c
+++ Part 1/wzip & wunzip/wzip.c
@@ -1,33 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
 int main(int argc, char *argv[]){
-
 	if(argc < 2){
-		printf("my-zip: file1 [file2 ...] \n");
+		printf("wzip: file1 [file2 ...] \n");
 		exit(1);
 	}
-
 	for(int filenum = 1; filenum < argc; filenum++){
-
 		FILE *fp = fopen(argv[filenum], "r");
-
 		if(fp == NULL){
-			printf("cannot open file\n");
+			printf("wzip: cannot open file\n");
 			exit(1);
 		}
-
 		char buf;
 		buf = fgetc(fp);
 		int count = 1;
 		char temp = buf;
 		buf = fgetc(fp);
-
+		
 		while(buf != EOF){
-
 			if(buf == temp){
 				count++;
 			}
-
 			else{
 				fwrite(&count, sizeof(int), 1, stdout);
 				fwrite(&temp, sizeof(temp), 1, stdout);
@@ -36,7 +29,6 @@ int main(int argc, char *argv[]){
 			}
 			buf = fgetc(fp);
 		}
-
 		fclose(fp);
 	}
 	return(0);
